5cdb5ae4a69fc55bc8038bae9dabd61e5a8fb655
santosh yadav <santosh@bookyourpet.com>
2024-08-17 13:47:55 +0530

    added class cmp

---
+++ src/localization/Convert/Screens/Classcmp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { TouchableOpacity, View } from 'react-native';
+
+class Classcmp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'santosh',
+      data: [],
+    };
+  }
+
+  render() {
+    return <View></View>;
+  }
+}
+
+export default Classcmp;
