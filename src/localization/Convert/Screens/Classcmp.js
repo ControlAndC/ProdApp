75fba80700194a6d38fca2a606807c53177bdc82
santosh yadav <santosh@bookyourpet.com>
2024-08-19 22:05:50 +0530

    added sace

--- src/localization/Convert/Screens/Classcmp.js
+++ src/localization/Convert/Screens/Classcmp.js
@@ -5,6 +5,9 @@ class Classcmp extends Component {
   constructor(props) {
     super(props);
     this.state = {
+
+
+      
       name_: 'santosh        xyz ',
       data: [1, 2, 3, 4, 5, 6, 7, 8],
     };
